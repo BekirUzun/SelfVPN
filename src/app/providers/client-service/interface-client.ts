0621cc32db9f3661a53a6489d9c6234d357b8d04
Bekir Uzun <bekiruzun8000@gmail.com>
2019-09-14 17:12:28 +0300

    cleanup for gh-pages

--- src/app/providers/client-service/interface-client.ts
+++
@@ -1,7 +0,0 @@
-export interface IClient {
-    connect(): Promise<any>;
-    disconnect(): Promise<any>;
-    isConnected(): Promise<boolean>;
-    startNetworkMonitor(outputHandler: (output: string) => void): Promise<any>;
-    stopNetworkMonitor();
-}
--- src/app/shared/errors.ts
+++
@@ -1,3 +0,0 @@
-export enum errors {
-    DROPLET_LIMIT = 'Your account have 0 droplet limit. It may be locked.',
-}
